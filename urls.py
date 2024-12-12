43ab8d4751368496d31c6d306e7b0518fbf49510
praneeth552 <112244566+praneeth552@users.noreply.github.com>
2024-12-12 22:23:23 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,23 @@
-from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+"""
+URL configuration for Content project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
 
 urlpatterns = [
-    path('', views.display, name='login'),
-    path('register/', views.register, name='register'),
-    path('forget/', views.forget, name='forget'),
-    path('otp/', views.otp, name='otp'),
-    path('verify-otp/', views.verify),
-    path('change/', views.change),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('logout/', views.logout_view, name='logout'),
-    path('upload-image/', views.upload_image, name='upload_image'),
-    path('myposts/',views.myposts,name='myposts'),
-    path('like/<int:post_id>/', views.like_post, name='like_post'),
-    path('comment/<int:post_id>/', views.add_comment, name='add_comment'),
-
-
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('',include('first.urls')),
+]

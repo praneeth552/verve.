0800ac5f54fc3d2752d0bc6678ca742f5d4032eb
praneeth552 <112244566+praneeth552@users.noreply.github.com>
2024-12-17 15:45:15 +0530

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for Content project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+# Add your project to the system path
+sys.path.append('/Desktop/Project')
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Content.settings')
+
+application = get_wsgi_application()
